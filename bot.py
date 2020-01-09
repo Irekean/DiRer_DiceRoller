7b59cf9edb4dd7655cfd8083457b6fd774850301
Irekean <gambellifulvio@gmail.com>
2020-01-09 17:52:29 +0100

    Everything is now under the Bot class, next step will be the dice class

--- bot.py
+++ bot.py
@@ -17,38 +17,28 @@ class Bot:
 
     def get_response(self):
         if self.admin:
-            if self.__is_message_command("kill") or self.__is_message_command("stop"):
+            if self.__is_command__("kill") or self.__is_command__("stop"):
                 await self.client.logout()
                 return None
-            elif self.__is_message_command("update"):
+            elif self.__is_command__("update"):
                 return "You got me! But I am still developing this part."
 
-    def __is_message_command(self, command):
-        return self.message.lower().startswith(command)
-
-
-def get_response(message, admin_right):
-    if message.content.strip().lower().startswith(PREFIX + "usage") or message.content.lower().startswith("/help"):
-        await message.channel.send("""Hi there, you can find the working functionality:
+        if self.__is_command__("usage") or self.__is_command__("help"):
+            return """Hi there, you can find the working functionality:
         - `""" + PREFIX + """r` : roll some dices using the format: `1d20+10`
         - `""" + PREFIX + """rf` : roll a D&D/Pathfinder full attack! Use the format: `1d20+15/+10/+5` WORK IN PROGRESS
         - `""" + PREFIX + """git` : get the GitHub project link
 
     If something does not work please open an issue on GitHub: """ + get_value("CustomBotUpdates",
-                                                                               "UpdateRepository") + """/issues""")
-    elif message.content.strip().lower().startswith(PREFIX + 'rf '):
-        await message.channel.send(roll_full_attack(message))
-    elif message.content.strip().lower().startswith(PREFIX + 'r ') or message.content.strip().lower().startswith(
-            PREFIX + 'roll '):
-        result = Dice(message).roll()
-        await message.channel.send(result)
-    elif (message.content.lower().strip() + " ").startswith(PREFIX + "git "):
-        await message.channel.send(
-            "The bot code is on GitHub: " + get_value("CustomBotUpdates", "UpdateRepository"))
-    elif is_this_admin(message.author.id) and (
-            message.content.strip().lower().startswith(
-                PREFIX + 'kill') or message.content.strip().lower().startswith(
-        PREFIX + 'stop')):
-        await client.logout()
-    elif message.content.strip().startswith(PREFIX):  # Always keep as last
-        await message.channel.send("Need help? Try with `" + PREFIX + "help` or `" + PREFIX + "usage`")
+                                                                               "UpdateRepository") + """/issues"""
+        elif self.__is_command__("rf "):
+            return roll_full_attack(self.message)
+        elif self.__is_command__("r ") or self.__is_command__("roll "):
+            return Dice(self.message).roll()
+        elif self.__is_command__("git"):
+            return "The bot code is on GitHub: " + get_value("CustomBotUpdates", "UpdateRepository")
+        elif self.__is_command__(""):
+            return "Need help? Try with `" + PREFIX + "help` or `" + PREFIX + "usage`"
+
+    def __is_command__(self, command):
+        return self.message.lower().startswith(PREFIX + command)
