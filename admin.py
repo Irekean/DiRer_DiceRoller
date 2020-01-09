ff7d316bb7f7ee65dbdc0d5ac97a8fb54fb25522
Irekean <gambellifulvio@gmail.com>
2020-01-09 16:09:28 +0100

    Added file for handling admin right

---
+++ admin.py
@@ -0,0 +1,31 @@
+from settings import get_value
+
+setup_done = False
+admin_list = None
+
+
+# Returns the list of the ids of the admin
+def get_admin_list():
+    global admin_list
+    __setup()
+    return admin_list
+
+git
+# Check if the variable is in the list of the admin
+def is_this_admin(possible_admin):
+    global admin_list
+    __setup()
+    return possible_admin in admin_list
+
+
+def __setup():
+    global setup_done
+    if not setup_done:
+        global admin_list
+        admin_list = __populate_list()
+        setup_done = True
+
+
+def __populate_list():
+    admin_raw = get_value("DEFAULT", "Admins")
+    return admin_raw.replace(" ", "").split(",")
